45a1c84ca4495ef2f49ac8c00818aa1765da790a
unknown <giogoncalvespadua13@gmail.com>
2026-03-26 09:09:00 -0300

    Aula 2

--- aula2/script.js
+++ aula2/script.js
@@ -4,7 +4,80 @@ function subtrai(d, e) {
     return d - e
 }
 
-Pode ser escrito assim: */
+Pode ser escrito assim:
 const subtrai = (b, c) => b - c;
 
 alert(subtrai(20, 10))
+
+
+function criarContador() {
+    let contagem = 0
+
+    return {
+        incrementar: () => {
+            contagem++
+            console.log(contagem)
+        },
+        valorAtual: () => contagem
+    }
+}
+
+/* Dessa forma, retorna apenas 1
+criarContador().incrementar()
+criarContador().incrementar()
+
+const contador = criarContador()
+contador.incrementar()
+contador.incrementar()
+*/
+
+// Programação assíncrona
+
+function callback(erro, resultado) {
+    if(erro != null) {
+        console.log(`Houve um erro: ${erro}`)
+        return
+    }
+    console.log(resultado)
+}
+
+function buscarUsuario(callback) {
+    setTimeout(() => {
+        callback(null, {
+            nome: "André",
+            idade: 31
+        }),
+        3000
+    })
+}
+
+//buscarUsuario(callback);
+
+/*
+fetch("https://viacep.com.br/ws/19025000/json") // requisição da url
+    .then(result => result.json())
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
+
+*/
+
+async function pegarCep(cep) {
+    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+    const resultado = await resposta.json()
+    //console.log(resultado)
+    return resultado
+}
+
+//console.log(pegarCep("httsasfc")) // retorna uma promisse
+//   .then(result => console.log(result))
+
+const executar = async (cep) => {
+    try {
+        return await pegarCep(cep)
+    } 
+    catch (error) {
+        return error // captar o erro e retorná-lo
+    }
+}
+
+executar("19025000").then(result => console.log(result))
