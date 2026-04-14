c663880bb5160a9ca54479a889a1686524756732
“giovana-padua” <giogoncalvespadua13@gmail.com>
2026-04-14 15:59:20 -0300

    Atividade 4 feita

--- at4/assets/script/script.js
+++ at4/assets/script/script.js
@@ -1,79 +1,134 @@
-
-// inicializar variável contadora
-let total = 0
-
 //import {Pessoa} from './Pessoa.js'; // Não funcionou
 
 // Classe Pessoa
 class Pessoa {
     constructor(genero, caminhoImg, quantidade) {
-        this.genero = genero
-        this.icone = caminhoImg
+        this.genero = genero.toLowerCase()
+        this.icone = caminhoImg.toLowerCase()
         this.quantidade = quantidade
     }
 
     somar() {
         this.quantidade++
+        return this.quantidade
     }
 
     subtrair() {
+        if (this.quantidade == 0)
+            return 0
+
         this.quantidade--
+        return this.quantidade
+    }
+
+    zerar() {
+        this.quantidade = 0
+        return this.quantidade
     }
 }
 
-// Criando objetos
+// Instância dos objetos no vetor
 const vet_pessoas = [
-    new Pessoa('Homens', './assets/img/freepik_man.png', 0),
-    new Pessoa('Mulheres', './assets/img/freepik_woman.png', 0)
+    new Pessoa('homens', './assets/img/freepik_man.png', 0),
+    new Pessoa('mulheres', './assets/img/freepik_woman.png', 0)
 ]
 
-// Adicionando os elementos HTML
-document.querySelector("#container").innerHTML = 
-    `<h1>
-        Total
-        <button class="redefinir" id="redefinir" onclick=''></button>
-    </h1>
-    <p id="total" class=qtd></p>
-    <div id="contadores">
+// Elementos HTML
+// Estrutura básica
+document.querySelector("#container").innerHTML = `
+    <div class="top">
+        <h1 class="title">
+            Total
+            <button class="redefinir" id="redefinir" onclick="${zerar()}">
+                <img src='./assets/img/redefinir.png' title='https://www.flaticon.com/br/icones-gratis/redefinir'>
+            </button>
+        </h1>
+        <p id="total" class=qtd>${calcularTotal()}</p>
     </div>
-    `
 
-// Adicionando objetos no HTML
+    <div id="contadores"></div>`
+
+// Div de cada Objeto
 for (let i = 0; i < vet_pessoas.length; i++) 
 {
-    total += vet_pessoas[i].quantidade
     document.querySelector("#contadores").insertAdjacentHTML(
-        'beforeend', 
-        `
-        <div class="pessoa">
+        'beforeend', `
+        <div class="pessoa" id=${vet_pessoas[i].genero}>
             <img src="${vet_pessoas[i].icone}">
     
             <div>
-                <button class="somar" onclick="${somar(this)}">+</button>
-                <button class="subtrair" onclick="${subtrair(this)}">-</button>
+                <button class="somar">+</button>
+                <button class="subtrair">-</button>
             </div>
     
-            <p>${vet_pessoas[i].genero}</p>
+            <p class="genero">${vet_pessoas[i].genero}</p>
     
             <p class="qtd">${vet_pessoas[i].quantidade}</p>
-        </div>
-        `
-    )
+        </div>`)
 }
 
-document.querySelector('#total').innerHTML = total
-
-
-function somar(button) {
-    console.log(button)
+// Funções
+function somar(gen) {
+    if (gen == 'h')
+        return vet_pessoas[0].somar()
+    else
+        return vet_pessoas[1].somar()
 }
 
-function subtrair(button) {
-    console.log(button)
+function subtrair(gen) {
+    if (gen == 'h')
+        return vet_pessoas[0].subtrair()
+    else
+        return vet_pessoas[1].subtrair()
 }
 
-function zerar() {
+function zerar(gen) {
+    if (gen == 'h')
+        return vet_pessoas[0].zerar()
+    else
+        return vet_pessoas[1].zerar()
+}
+
+function calcularTotal() {
+    let total = 0
     for (let i = 0; i < vet_pessoas.length; i++) {
-        vet_pessoas[i].quantidade = 0
+        total += vet_pessoas[i].quantidade
     }
+    return total
 }
+
+// Processamento
+// Eventos dos botões
+const homens_btns = document.querySelector('#homens').querySelectorAll('button')
+const mulheres_btns = document.querySelector('#mulheres').querySelectorAll('button')
+const redefinir_btn = document.querySelector('#redefinir')
+
+const homens_total = document.querySelector('#homens').querySelector('.qtd')
+const mulheres_total = document.querySelector('#mulheres').querySelector('.qtd')
+const total_pessoas = document.querySelector('#total')
+
+homens_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.className == 'somar')
+            homens_total.innerHTML = somar('h')
+        else
+            homens_total.innerHTML = subtrair('h')
+        total_pessoas.innerHTML = calcularTotal()
+    })
+})
+
+mulheres_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.className == 'somar')
+            mulheres_total.innerHTML = somar('m')
+        else
+            mulheres_total.innerHTML = subtrair('m')
+        total_pessoas.innerHTML = calcularTotal()
+    })
+})
+
+redefinir_btn.addEventListener('click', () => {
+    homens_total.innerHTML = zerar('h')
+    mulheres_total.innerHTML = zerar('m')
+    total_pessoas.innerHTML = calcularTotal()
+})
